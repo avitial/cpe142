4b4328e7f2e2c5ec52d4329b28b19ede8aa42a56
leavitia <lavitia9@gmail.com>
2016-11-22 13:18:19 -0800

    Adjusted the register file module to meet the project's design requirements. Still missing read and write R15 register, will add that on the next commit.

--- registerFile.v
+++ registerFile.v
@@ -1,36 +1,30 @@
 // Reference from http://courses.cs.washington.edu/courses/cse370/10sp/pdfs/lectures/regfile.txt 
-// This is a Verilog description for an 8 x 16 register file
+// This is a Verilog description for a 16 x 16-bit register file
 
 `timescale 1ns / 1ns
 
 module registerFile
-  (input clk,
-   input reset,
-   input write,
-   input [2:0] wrAddr,
-   input [15:0] wrData,
-   input [2:0] rdAddrA,
-   output [15:0] rdDataA,
-   input [2:0] rdAddrB,
-   output [15:0] rdDataB);
+	(input clk,
+	input rst,
+	input wr,
+	input [2:0] wrAddr,
+	input [15:0] wrData,
+	input [2:0] rdAddrR1,
+	output [15:0] rdDataR1,
+	input [2:0] rdAddrR2,
+	output [15:0] rdDataR2);
 
-   reg [15:0] 	 regfile [0:7];
+	integer i; 
+	reg [15:0] regfile [0:15];
 
-   assign rdDataA = regfile[rdAddrA];
-   assign rdDataB = regfile[rdAddrB];
+	assign rdDataR1 = regfile[rdAddrR1];
+	assign rdDataR2 = regfile[rdAddrR2];
 
-   always @(posedge clk) begin
-      if (reset) begin
-	 regfile[0] <= 0;
-	 regfile[1] <= 0;
-	 regfile[2] <= 0;
-	 regfile[3] <= 0;
-	 regfile[4] <= 0;
-	 regfile[5] <= 0;
-	 regfile[6] <= 0;
-	 regfile[7] <= 0;
-      end else begin
-	 if (write) regfile[wrAddr] <= wrData;
-      end // else: !if(reset)
-   end
+	always @(posedge clk) begin
+	if (rst) begin
+		for(i=0; i<16; i=i+1) regfile[i] <= 0;
+	end else begin
+	if (wr) regfile[wrAddr] <= wrData;
+	end // else: !if(rst)
+	end
 endmodule
