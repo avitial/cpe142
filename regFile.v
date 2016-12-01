6e7968846aeb230522e9a9ef75547efcd9564e26
leavitia <lavitia9@gmail.com>
2016-12-01 12:47:22 -0800

    Made changes to regFile, adding logic to signals.

--- regFile.v
+++ regFile.v
@@ -6,13 +6,15 @@ module regFile (clk, rst, wr, wrAddr, wrData, rdAddrR1, rdDataR1, rdAddrR2, rdDa
 	input clk;
 	input rst;
 	input wr;
-	input [2:0] wrAddr;
-	input [15:0] wrData;
-	input [2:0] rdAddrR1;
+	input wrR15;
+	input [2:0]   wrAddr;
+	input [15:0]  wrData;
+	input [15:0]  wrDataR15;
+	input [2:0]   rdAddrR1;
 	output [15:0] rdDataR1;
-	input [2:0] rdAddrR2;
+	input [2:0]   rdAddrR2;
 	output [15:0] rdDataR2;
-	input [2:0] rdAddrR15;
+	input [2:0]   rdAddrR15;
 	output [15:0] rdDataR15;
 
 	parameter regSize=4;
@@ -27,7 +29,7 @@ module regFile (clk, rst, wr, wrAddr, wrData, rdAddrR1, rdDataR1, rdAddrR2, rdDa
 	assign rdDataR15 = regfile[rdAddrR15];
 	
 	reg[dataSize-1:0]regData[regCnt-1:0];
-	integer i;
+	integer n;
 	
 	always @(posedge clk)
 	begin
@@ -36,22 +38,50 @@ module regFile (clk, rst, wr, wrAddr, wrData, rdAddrR1, rdDataR1, rdAddrR2, rdDa
 			for(i=0; i<regCnt; i=i+1)
 			begin
 				case(i)
-					1:regData[i]<=16'hFFFF;
-					2:regData[i]<=16'h0050;
-					3:regData[i]<=16'hF033;
-					4:regData[i]<=16'hF0FF;
-					5:regData[i]<=16'h0040;
-					6:regData[i]<=16'h6666;
-					7:regData[i]<=16'h00FF;
-					8:regData[i]<=16'h8888;
-					9:regData[i]<=16'h0000;
-					10:regData[i]<=16'h0000;
-					11:regData[i]<=16'h0000;
-					12:regData[i]<=16'hCCCC;
-					13:regData[i]<=16'h0002;
-					default:regData[i]<=16'h0000;
+					1:regData[n]<=16'hFFFF;
+					2:regData[n]<=16'h0050;
+					3:regData[n]<=16'hF033;
+					4:regData[n]<=16'hF0FF;
+					5:regData[n]<=16'h0040;
+					6:regData[n]<=16'h6666;
+					7:regData[n]<=16'h00FF;
+					8:regData[n]<=16'h8888;
+					9:regData[n]<=16'h0000;
+					10:regData[n]<=16'h0000;
+					11:regData[n]<=16'h0000;
+					12:regData[n]<=16'hCCCC;
+					13:regData[n]<=16'h0002;
+					default:regData[n]<=16'h0000;
 				endcase
 			end 
 		end
+		else
+		begin
+			case({wr,wrR15})
+				2'b00:
+				begin
+				end
+				2'b01:
+				begin
+					regData[{regSize{1'b0}}]<=wrDataR15;
+				end
+				2'b10:
+				begin
+					regData[wrAddr]<=wrData;
+				end
+				2'b11:
+				begin
+					if(wrAddr=={regSize{1'b0}})
+					begin
+						regData[wrAddr]<=wrData;
+					end
+					else
+					begin
+						regData[wrAddr]<=wrData;		
+						regData[{dataSize{1'b0}}]<=wrDataR15;
+					end
+				end
+			endcase
+		end
 	end
-  
+endmodule
