5cdda3cea5bfb11fb6e2680bf4aebdae84c5d277
Luis E Avitia <lavitia9@gmail.com>
2016-12-03 02:03:05 -0800

    mux2to1.v module completed. Missing testbench.
    2 to 1 multiplexer module completed, still missing testbench. Testbench will be performed once all modules of datapath have been completed.

--- mux2to1.v
+++ mux2to1.v
@@ -1,4 +1,18 @@
-module mux2to1();
+/* 2 to 1 multiplexer module (mux2to1.v) outputs the corresponding signal based on the selection signal received. 
+*/
 
-
+module mux2to1(a, b, out, sel);
+	input a, b, sel;
+	output out;
+	reg out;
+  always @ (a or b or sel)
+	begin
+		case(sel)
+			0: sel = a;
+			1: sel = b;
+			`ifdef DEBUG
+				default: $display("Debug msg: there was an error triggered in the program.");
+			`endif
+		endcase
+	end
 endmodule
