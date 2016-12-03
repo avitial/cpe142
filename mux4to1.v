13b83d2736df5d0c507e48701733ac3abf0b690d
Luis E Avitia <lavitia9@gmail.com>
2016-12-03 01:59:05 -0800

    mux4to1.v module completed. Missing testbench.
    4 to 1 multiplexer module completed, still missing testbench. Testbench will be performed once all the modules of the datapath have been completed.

--- mux4to1.v
+++ mux4to1.v
@@ -1,3 +1,21 @@
-module mux4to1();
+/* 4 to 1 multiplexer module (mux4to1.v) outputs the corresponding signal based on the selection signal received. 
+*/
 
+module mux4to1(a, b, c, d, out, sel);
+	input a, b, c, d;
+	input [1:0] sel;
+	output out;
+	reg out;
+	always @ (a or b or c or d or sel)
+	begin
+		case(sel)
+			0: sel = a;
+			1: sel = b;
+			2: sel = c;
+			3: sel = d;
+			`ifdef DEBUG
+				default: $display("Debug msg: there was an error triggered in the program.");
+			`endif
+		endcase
+	end
 endmodule
