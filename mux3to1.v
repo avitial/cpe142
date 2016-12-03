6cec20343ddcb3538724306fbc71934e1c842da6
Luis E Avitia <lavitia9@gmail.com>
2016-12-03 02:00:58 -0800

    mux3to1.v module completed. Missing testbench.
    3 to 1 multiplexer module completed, still missing testbench. Testbench will be performed once all the modules of datapath have been completed.

--- mux3to1.v
+++ mux3to1.v
@@ -1,3 +1,19 @@
-module mux3to1();
-
+/* 3 to 1 multiplexer module (mux4to1.v) outputs the corresponding signal based on the selection signal received. 
+*/
+module mux3to1(a, b, c, out, sel);
+	input a, b, c;
+	input [1:0] sel;
+	output out;
+	reg out;
+	always @ (a or b or c or sel)
+	begin
+		case(sel)
+			0: sel = a;
+			1: sel = b;
+			2: sel = c;
+			`ifdef DEBUG
+				default: $display("Debug msg: there was an error triggered in the program.");
+			`endif
+		endcase
+	end
 endmodule
