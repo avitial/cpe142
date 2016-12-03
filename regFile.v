5a5307739a1cfdaeecb2c07087fee0d24cf37ab3
Luis E Avitia <lavitia9@gmail.com>
2016-12-03 00:39:45 -0800

    regFile.v final update.
    Still missing testbench, will perform once all modules of datapath are completed

--- regFile.v
+++ regFile.v
@@ -1,33 +1,34 @@
-/* Reference from http://courses.cs.washington.edu/courses/cse370/10sp/pdfs/lectures/regfile.txt 
-   This is a Verilog description for a 16 x 16-bit register file */
-`timescale 1ns / 1ns
-
+/* This Verilog module (regFile.v)  represents the register file of the pipelined datapath. The function this module performs in the 
+   datapath includes the handling of read and write operations for the registers mentioned in the project design file. The module makes 
+   use of some control signals to enable read and write operations and the set of processor registers found in the CPU. This module 
+   also contains multiple ports for read and write operations and a set of registers used to phase data in between the memory and other 
+   sections.
+*/
 module regFile(clk,rst,regR1,regR2,regDst,regDstData,regR15Data,wr,wrR15,rdR1,rdR2,rdR15);
-	parameter dataSize=16;
 	parameter regSize=4;
+	parameter dataSize=16;
 	parameter regCnt=(1<<regSize);
 	integer n;
-
+	
 	input clk,rst;
 	input [regSize-1:0] regR1;
 	input [regSize-1:0] regR2;
 	input [regSize-1:0] regDst;
-	
-	input wr,wrR15;
 	input [dataSize-1:0] regDstData;
 	input [dataSize-1:0] regR15Data;
+	input wr,wrR15;
 	
-	output reg[dataSize-1:0]rdR1;
-	output reg[dataSize-1:0]rdR2;
-	output reg[dataSize-1:0]rdR15;
-
+	output reg[dataSize-1:0] rdR1;
+	output reg[dataSize-1:0] rdR2;
+	output reg[dataSize-1:0] rdR15;
+	
 	reg[dataSize-1:0]regData[regCnt-1:0];
-	
+
 	always@(negedge rst or posedge clk)
 	begin
 		if(!rst)
 		begin
-			for(n=0;n<regCnt;n=n+1)
+			for(n=0; n<regCnt; n=n+1)
 			begin
 				case(n)
 					1:regData[n]<=16'hFFFF;
@@ -49,34 +50,35 @@ module regFile(clk,rst,regR1,regR2,regDst,regDstData,regR15Data,wr,wrR15,rdR1,rd
 		end
 		else
 		begin
-		case({wr,wrR15})
-			2'b00:
-			begin
-				/* Do Nothing */
-			end
-			2'b01:
-			begin
-				regData[{regSize{1'b0}}]<=wrR15;
-			end
-			2'b10:
-			begin
-			regData[regDst]<=regDstData;
-			end
-			2'b11:
-			begin
-				if(regDst=={regSize{1'b0}})
+			case({wr15,wr})
+				2'b00:
+				begin
+					// Do nothing
+				end
+				2'b01:
 				begin
 					regData[regDst]<=regDstData;
 				end
-				else
+				2'b10:
 				begin
-					regData[regDst]<=regDstData;		
-					regData[{dataSize{1'b0}}]<=wrR15;
+					regData[{regSize{1'b0}}]<=regR15Data;
 				end
-			end
-		endcase
+				2'b11:
+				begin
+					if(regDst=={regSize{1'b0}})
+					begin
+						regData[regDst]<=regDstData;
+					end
+					else
+					begin
+						regData[regDst]<=regDstData;		
+						regData[{dataSize{1'b0}}]<=regR15Data;
+					end
+				end
+			endcase
 		end
 	end
+
 	always@(*)
 	begin
 		rdR1=regData[regR1];
