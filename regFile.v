947738aa351210c9b0d402205a61d515109ea93c
Luis E Avitia <lavitia9@gmail.com>
2016-12-04 11:27:02 -0800

    register file module completed and tested.

--- regFile.v
+++ regFile.v
@@ -5,79 +5,77 @@
    sections of the datapath.
 */
 module regFile(clk,rst,regR1,regR2,regDst,regDstData,regR15Data,wr,wrR15,rdR1,rdR2,rdR15);
-        // Variables
-		parameter regCnt=16;
-        integer n;
-        integer regR15=15;
+	// Variables
+	parameter regCnt=16;
+	integer n;
+	integer regR15=15;
+	// Input ports
+	input clk,rst, wr, wrR15;
+	input [3:0] regR1;
+	input [3:0] regR2;
+	input [3:0] regDst;
+	input [15:0] regDstData;
+	input [15:0] regR15Data;
+	//Output ports
+	output reg[15:0] rdR1;
+	output reg[15:0] rdR2;
+	output reg[15:0] rdR15;
 
-		// Input ports
-        input clk,rst, wr, wrR15;
-        input [3:0] regR1;
-        input [3:0] regR2;
-        input [3:0] regDst;
-        input [15:0] regDstData;
-        input [15:0] regR15Data;
+	reg[15:0]regData[regCnt:0];
 
-		//Output ports
-        output reg[15:0] rdR1;
-        output reg[15:0] rdR2;
-        output reg[15:0] rdR15;
-
-        reg[15:0]regData[regCnt:0];
-
-        always@(posedge clk or negedge rst)
-        begin
-			if(!rst)
+	always@(posedge clk or negedge rst)
+	begin
+		if(!rst)
+		begin
+			for(n=0; n<regCnt; n=n+1)
 			begin
-				for(n=0; n<regCnt; n=n+1)
-				begin
-					case(n)
-							1:regData[n]<=16'hFFFF;
-							2:regData[n]<=16'h0050;
-							3:regData[n]<=16'hF033;
-							4:regData[n]<=16'hF0FF;
-							5:regData[n]<=16'h0040;
-							6:regData[n]<=16'h6666;
-							7:regData[n]<=16'h00FF;
-							8:regData[n]<=16'h8888;
-							9:regData[n]<=16'h0000;
-							10:regData[n]<=16'h0000;
-							11:regData[n]<=16'h0000;
-							12:regData[n]<=16'hCCCC;
-							13:regData[n]<=16'h0002;
-							default:regData[n]<=16'h0000;
-					endcase
-				end
-			end
-			else
-			begin
-				case({wr,wrR15})
-						2'b00:
-						begin
-							// Do nothing
-						end
-						2'b10:
-						begin
-							regData[regDst]<=regDstData;
-						end
-						2'b01:
-						begin
-							regData[regR15]<=regR15Data;
-						end
-						 2'b11:
-						begin
-							if(regDst=={{4'b0}})
-							begin
-									regData[regDst]<=regDstData;
-							end
-							else
-							begin
-									regData[regDst]<=regDstData;
-									regData[regR15]<=regR15Data;
-							end
-						end
+				case(n)
+					1:regData[n]<=16'hFFFF;
+					2:regData[n]<=16'h0050;
+					3:regData[n]<=16'hF033;
+					4:regData[n]<=16'hF0FF;
+					5:regData[n]<=16'h0040;
+					6:regData[n]<=16'h6666;
+					7:regData[n]<=16'h00FF;
+					8:regData[n]<=16'h8888;
+					9:regData[n]<=16'h0000;
+					10:regData[n]<=16'h0000;
+					11:regData[n]<=16'h0000;
+					12:regData[n]<=16'hCCCC;
+					13:regData[n]<=16'h0002;
+					default:regData[n]<=16'h0000;
 				endcase
 			end
+		end
+		else
+		begin
+			case({wr,wrR15})
+				2'b00:
+				begin
+					// Do nothing
+				end
+				2'b10:
+				begin
+					regData[regDst]<=regDstData;
+				end
+				2'b01:
+				begin
+					regData[regR15]<=regR15Data;
+				end
+				2'b11:
+				begin
+					if(regDst=={{4'b1}})
+					begin
+						regData[regDst]<=regDstData;
+					end
+					else
+					begin
+						regData[regDst]<=regDstData;
+						regData[regR15]<=regR15Data;
+					end
+				end
+			endcase
+		end
         end
 
         always@(*)
