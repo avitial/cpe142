6b7b092a86b3bd6cdac640a69a47259cbcd2d181
em2656 <ezequielmghs@yahoo.com>
2016-12-04 15:28:33 -0800

    95% complete. Missing testbench

--- hazDetnUnit.v
+++ hazDetnUnit.v
@@ -1,3 +1,22 @@
-module	hazDetnUnit();
+module	hazDetnUnit(regR1, regR2, EXregR2, rdR1rdR15comp, IDEXMemRead, PCWrite, IFIDWrite, halt);
+
+	input [3:0]regR1, regR2, EXregR2, rdR1rdR15comp;
+	input IDEXMemRead;
+	output PCWrite, IFIDWrite, halt;
+
+	reg PCWrite, IFIDWrite, halt;
+	always@(*)
+		if((regR1 == rdR1rdR15comp || regR2 == rdR1rdR15comp) && IDEXMemRead;
+			begin // halt
+				PCWrite = 1'b0;
+				IFIDWrite = 1'b0;
+				halt = 1'b1;
+			end
+		else
+			begin // no halt
+				PCWrite = 1'b1;
+				IFIDWrite = 1'b1;
+				halt = 1'b0;
+			end
 
 endmodule
