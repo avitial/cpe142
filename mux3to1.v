a9b18e6b04cc1ee03175beb0f371fd35b8358729
leavitia <lavitia9@gmail.com>
2016-12-04 16:08:00 -0800

    mux 3to1 ocmpleted

--- mux3to1.v
+++ mux3to1.v
@@ -1,10 +1,12 @@
 /* 3 to 1 multiplexer module (mux3to1.v) outputs the corresponding signal based on the selection signal received. 
 */
 module mux3to1(a, b, c, out, sel);
-	input a, b, c;
+	input [3:0] a;
+	input [3:0] b;
+	input [3:0] c;
 	input [1:0] sel;
-	output out;
-	reg out;
+	output [3:0] out;
+	reg [3:0] out;
 	always @ (sel or a or b or c)
 	begin
 		case(sel)
