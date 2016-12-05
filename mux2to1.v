2a828d252e43efb5968b682a2ad4602c04a6fb02
leavitia <lavitia9@gmail.com>
2016-12-04 20:57:40 -0800

    i

--- mux2to1.v
+++ mux2to1.v
@@ -1,6 +1,5 @@
 /* 2 to 1 multiplexer module (mux2to1.v) outputs the corresponding signal based on the selection signal received. 
 */
-
 module mux2to1(a, b, out, sel);
 	input a, b, sel;
 	output out;
