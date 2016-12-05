942ebfd8a1924168c1dff8d76ba4767fe8ce83c7
em2656 <ezequielmghs@yahoo.com>
2016-12-05 15:26:24 -0800

    Testbench for hazDetnUnit complete

--- testbench/hazDetnUnit_fixture.v
+++ testbench/hazDetnUnit_fixture.v
@@ -1,4 +1,33 @@
 'include "hazDetnUnit.v"
-module
 
+module hazDetnUnit_fixture;
+	
+	reg [3:0]regR1, regR2, EXregR2, rdR1rdR15comp;
+	reg IDEXMemRead;
+	wire PCWrite, IFIDWrite, halt;
+	
+	$monitor 
+
+	hazDetnUnit hazDetnUnit1(regR1,regR2,EXregR2,rdR1rdR15comp,IDEXMemRead,PCWrite, 		IFIDWrite, halt);
+	
+	initial
+	begin
+		regR1
+		regR2
+		rdR1rdR15comp
+		
+		#20 IDEXMemRead =1'b0;
+		#20 IDEXMemRead =1'b1;
+		#20 regR2 =4'b1100;
+		#20 regR2=4'b0000;
+		#20 regR1=4'b1100;
+		#20 IDEXMemRead =1'b0;
+
+	end
+
+	initial
+		begin
+			#800
+			$finish;
+		end
 endmodule
