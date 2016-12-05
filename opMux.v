225c84bbc3d959dba7dcafbd9a9e41bcee61c2b3
leavitia <lavitia9@gmail.com>
2016-12-04 19:42:42 -0800

    3 to 1 multiplexer for ALU Src operators. Testbench pending.

---
+++ opMux.v
@@ -0,0 +1,25 @@
+/* 3 to 1 multiplexer module (opMux.v) opputs the corresponding operator for the ALU inputs based on 
+   the ALUSrcection signal received by the forwarding unit. 
+*/
+module opMux(a, b, c, op, ALUSrc);
+	// Input Ports
+	input [1:0] ALUSrc;
+	input [15:0] a;
+	input [15:0] b;
+	input [15:0] c;
+	// Output Ports
+	output [15:0] op;
+	reg [15:0] op;
+
+	always @ (ALUSrc or a or b or c)
+	begin
+		case(ALUSrc)
+			0: op = a;
+			1: op = b;
+			2: op = c;
+			`ifdef DEBUG
+				default: $display("Debug msg: there was an error triggered in the program.");
+			`endif
+		endcase
+	end
+endmodule
