0eb64896b6203bd976f0f5937a58cbdbcf8124a2
Luis E Avitia <lavitia9@gmail.com>
2016-12-05 21:43:58 -0800

    Update hazDetnUnit.v

--- hazDetnUnit.v
+++ hazDetnUnit.v
@@ -1,20 +1,20 @@
-module	hazDetnUnit(regR1, regR2, EXregR2, rdR1rdR15comp, IDEXMemRead, PCWrite, IFIDWrite, halt);
+module	hazDetnUnit(regR1, regR2, EXregR2, rdR1rdR15comp, IDEXMemRead, wrPC, IFIDWrite, halt);
 
 	input [3:0]regR1, regR2, EXregR2, rdR1rdR15comp;
 	input IDEXMemRead;
-	output PCWrite, IFIDWrite, halt;
+	output wrPC, IFIDWrite, halt;
 
-	reg PCWrite, IFIDWrite, halt;
+	reg wrPC, IFIDWrite, halt;
 	always@(*)
 		if((regR1 == rdR1rdR15comp || regR2 == rdR1rdR15comp) && IDEXMemRead;
 			begin // halt
-				PCWrite = 1'b0;
+				wrPC = 1'b0;
 				IFIDWrite = 1'b0;
 				halt = 1'b1;
 			end
 		else
 			begin // no halt
-				PCWrite = 1'b1;
+				wrPC = 1'b1;
 				IFIDWrite = 1'b1;
 				halt = 1'b0;
 			end
